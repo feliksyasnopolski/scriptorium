5f43ffd2bef2577e7eb9620004e8a1624832057a
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2026-09-13 10:24:07 +0200

    polish: refine topbar grouping

--- frontend/src/components/TopBar.vue
+++ frontend/src/components/TopBar.vue
@@ -10,14 +10,16 @@ const theme = useTheme()
   <header class="topbar">
     <button class="wordmark" type="button" @click="$emit('home')">Scriptorium</button>
     <span v-if="projectOpen" class="save-state">{{ saveState }}</span>
-    <label class="theme-selector">Theme
-      <select aria-label="Theme" :value="theme.preference.value" @change="theme.setPreference(($event.target as HTMLSelectElement).value as ThemePreference)">
-        <option value="system">System</option>
-        <option value="light">Light</option>
-        <option value="dark">Dark</option>
-      </select>
-    </label>
-    <button class="account-button" type="button" @click="$emit('account')">Account</button>
-    <button class="logout-button" type="button" @click="$emit('logout')">Log out</button>
+    <div class="topbar-actions">
+      <label class="theme-selector">
+        <select aria-label="Theme" :value="theme.preference.value" @change="theme.setPreference(($event.target as HTMLSelectElement).value as ThemePreference)">
+          <option value="system">System</option>
+          <option value="light">Light</option>
+          <option value="dark">Dark</option>
+        </select>
+      </label>
+      <button class="account-button" type="button" @click="$emit('account')">Account</button>
+      <button class="logout-button" type="button" @click="$emit('logout')">Log out</button>
+    </div>
   </header>
 </template>
